82275b0792c6dd0bae40b33d924dd4ed374ea520
vincentcote87 <vincentcote87@gmail.com>
2018-11-26 13:43:53 -0700

    Changed to a single click listener

--- js/main.js
+++ js/main.js
@@ -4,20 +4,19 @@ const boxThreeText = '<h2>Watch me flip!</h2>';
 
 $(document).on('keypress', (pressed) => {
   switch (pressed.key) {
-    case '1': $('#item-one').toggleClass('green'); break;
-    case '2': $('#item-two').css('display') == 'none' ? $('#item-two').fadeIn(1000) : $('#item-two').fadeOut(1000); break;
-    case '3': $('#item-three').css('transform', 'rotate(180deg)'); break;
+  case '1': $('#item-one').toggleClass('green'); break;
+  case '2': $('#item-two').css('display') == 'none' ?
+    $('#item-two').fadeIn(1000) : $('#item-two').fadeOut(1000);
+    break;
+  case '3': $('#item-three').css('transform', 'rotate(180deg)'); break;
   }
 });
 
-$('#item-one').click(function() {
-  $(this).html(boxOneText);
-});
-
-$('#item-two').click(function() {
-  $(this).html(boxTwoText);
-});
-
-$('#item-three').click(function() {
-  $(this).html(boxThreeText);
-});
+$(document).on('click', function(target) {
+  let id = target.target.id;
+  switch (id) {
+  case "item-one": $('#' + id).html(boxOneText); break;
+  case "item-two": $('#' + id).html(boxTwoText); break;
+  case "item-three": $('#' + id).html(boxThreeText); break;
+  }
+})
