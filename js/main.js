a56e4c7d28fbd84812c1b840b9928859a02c42b2
vincentcote87 <vincentcote87@gmail.com>
2018-11-26 13:32:14 -0700

    Made changes as requested in assn doc

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-const GREEN = 'rgb(0, 128, 0)';
-const AQUAMARINE = 'rgb(127, 255, 212)';
-
-let boxOneText = '<h2>Is this green?!</h2>';
-let boxTwoText = '<h2>Can you see me?</h2>';
+const boxOneText = '<h2>Is this green?!</h2>';
+const boxTwoText = '<h2>Can you see me?</h2>';
+const boxThreeText = '<h2>Watch me flip!</h2>';
 
 $(document).on('keypress', (pressed) => {
   switch (pressed.key) {
-    case '1': updateColour(); break;
-    case '2': fadeBox(); break;
+    case '1': $('#item-one').toggleClass('green'); break;
+    case '2': $('#item-two').css('display') == 'none' ? $('#item-two').fadeIn(1000) : $('#item-two').fadeOut(1000); break;
     case '3': $('#item-three').css('transform', 'rotate(180deg)'); break;
   }
 });
@@ -21,42 +19,5 @@ $('#item-two').click(function() {
 });
 
 $('#item-three').click(function() {
-  $(this).html('<h2>Watch me flip!</h2>');
+  $(this).html(boxThreeText);
 });
-
-function updateColour() {
-  $('#item-one').css('background-color', function() {
-    let backColor = $(this).css('background-color');
-    if (backColor === GREEN) {
-      if ($(this).html() != "") {
-        boxOneText = '<h2>Is this green?!</h2>';
-        $(this).html(boxOneText);
-      }
-      return AQUAMARINE;
-    } else {
-      if ($(this).html() != "") {
-        boxOneText = '<h2>Now that\'s green!</h2>';
-        $(this).html(boxOneText);
-      }
-      return GREEN;
-      }
-  });
-}
-
-function fadeBox() {
-  $('#item-two').css('display', function() {
-    if ($(this).css('display') == 'none') {
-      if ($(this).html() != "") {
-        boxTwoText = '<h2>Can you see me?</h2>';
-        $(this).html(boxTwoText);
-      }
-      return $(this).fadeIn(1000);
-    } else {
-      if ($(this).html() != "") {
-        boxTwoText = '<h2>Noooooooo!!!!</h2>';
-        $(this).html(boxTwoText);
-      }
-      return $(this).fadeOut(1000);
-    }
-  })
-}
